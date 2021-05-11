839f5241e906b8e2fe7e0b290f677a04a047aac6
Victor <victor.czech@t-online.de>
2021-05-11 18:09:29 +0200

    Added uniforms to shaders

---
+++ shader.py
@@ -0,0 +1,53 @@
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileShader, compileProgram
+
+
+class ShaderProgram:
+
+    def __init__(self, shaders):
+
+        shaders = [compileShader(source, shader_type) for source, shader_type in shaders]
+        self.handle = compileProgram(*shaders)
+
+        self.uniforms = {}
+
+    def register_uniform(self, uniform_name, uniform_type):
+
+        loc = glGetUniformLocation(self.handle, uniform_name)
+
+        set_func = {
+            '1f': ShaderProgram.set_uniform_wrap(glUniform1f, loc, False),
+            '2f': ShaderProgram.set_uniform_wrap(glUniform2f, loc, False),
+            '3f': ShaderProgram.set_uniform_wrap(glUniform3f, loc, False),
+            '4f': ShaderProgram.set_uniform_wrap(glUniform4f, loc, False),
+            'mat3f': ShaderProgram.set_uniform_wrap(glUniformMatrix3fv, loc, True),
+            'mat4f': ShaderProgram.set_uniform_wrap(glUniformMatrix4fv, loc, True),
+        }[uniform_type]
+
+        self.uniforms[uniform_name] = (loc, uniform_type, set_func)
+
+    @staticmethod
+    def set_uniform_wrap(func, loc, is_matrix):
+
+        def wrapped(value):
+            if is_matrix:
+                return func(loc, 1, GL_FALSE, value)
+            else:
+                return func(loc, *value)
+
+        return wrapped
+
+    def set_uniform(self, uniform_name, value):
+        _, _, set_func = self.uniforms[uniform_name]
+        set_func(value)
+
+
+
+
+
+
+
+
+
+
+
